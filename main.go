5dc49cc29bc5afa2d166934568a4789f18bcf1f0
goodplayer <meidomx@gmail.com>
2016-04-28 22:46:05 +0800

    demo

--- main.go
+++ main.go
@@ -6,16 +6,23 @@ import (
 	"os/user"
 	"strconv"
 	"syscall"
+	"time"
 )
 
 func main() {
+	// user
+	// param
+	// env
+	// input/output/error
+	// exec
+
 	fmt.Println(user.Lookup("root"))
 	u, _ := user.Lookup("root")
 
 	attr := new(os.ProcAttr)
-	attr.Dir = "/sdf/sdf"
+	attr.Dir = "/tmp"
 	attr.Env = []string{} // env
-	//attr.Files = []*os.File{} // input/output/error
+	attr.Files = []*os.File{os.Stdin, os.Stdout, os.Stderr} // input/output/error
 	attr.Sys = new(syscall.SysProcAttr)
 	attr.Sys.Credential = new(syscall.Credential)
 	uid, _ := strconv.Atoi(u.Uid)
@@ -23,7 +30,9 @@ func main() {
 	attr.Sys.Credential.Uid = uint32(uid)
 	attr.Sys.Credential.Gid = uint32(gid)
 
-	p, err := os.StartProcess("xxx", []string{"xxx"}, attr)
+	p, err := os.StartProcess("go-import-server", []string{"20"}, attr)
 
 	fmt.Println(p, err)
+
+	time.Sleep(50 * time.Second)
 }
