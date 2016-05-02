45c095f3b3785c7548f6a86036264a6eedd77beb
goodplayer <meidomx@gmail.com>
2016-05-02 21:23:12 +0800

    basic features

--- README.md
+++ README.md
@@ -1,23 +1,44 @@
 # dstart
-start a program. for linux &amp; mac
 
-# TODO
+start a program as daemon service. for linux &amp; mac
+
 
 ####help
 
 ```
 dstart -h
-dstart -help
+dstart --help
 ```
 
 ####usage
 
 ```
-dstart [executable] [params...]
+dstart [options] executable [params...]
 ```
 
-####env
+####option usage
 
 ```
-dstart -env key=value [-env key=value ...] [executable] [params...]
+Usage of ./dstart:
+  -env value
+        -env key=value [-env key=value ...] (default array flags.)
+  -err string
+        -err error_output_file (currently overwriten file not create a new one)
+  -etoo
+        -etoo : redirect error output to standard output
+  -in string
+        -in input_file
+  -out string
+        -out output_file (currently overwriten file not create a new one)
+  -u string
+        -u username
+  -wd string
+        -wd working_directory
+```
+
+####example
+
+#####tail a file then write to another
+```
+./dstart -in ~/input_file -out ~/output_file -wd /usr/bin tail
 ```
--- main.go
+++ main.go
@@ -1,39 +1,120 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
 	"os"
+	"os/signal"
 	"os/user"
 	"strconv"
 	"syscall"
-	"time"
 )
 
-func main() {
-	// user
-	// param
-	// env
-	// input/output/error
-	// exec
-	// signal(SIGCHLD, SIG_IGN);
+var username string
+var wd string
+var input string
+var output string
+var erroroutput string
+var etoo bool
 
-	fmt.Println(user.Lookup("root"))
-	u, _ := user.Lookup("root")
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+	return "array flags."
+}
+
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+var env arrayFlags
+
+func init() {
+	flag.StringVar(&username, "u", "", "-u username")
+	flag.StringVar(&wd, "wd", "", "-wd working_directory")
+	flag.Var(&env, "env", "-env key=value [-env key=value ...]")
+	flag.StringVar(&input, "in", "", "-in input_file")
+	flag.StringVar(&output, "out", "", "-out output_file (currently overwriten file not create a new one)")
+	flag.StringVar(&erroroutput, "err", "", "-err error_output_file (currently overwriten file not create a new one)")
+	flag.BoolVar(&etoo, "etoo", false, "-etoo : redirect error output to standard output")
+}
+
+// dstart [options] executable [params...]
+func main() {
+	flag.Parse()
+
+	if len(flag.Args()) <= 0 {
+		log.Fatalln("should have executable file. illegal argument. usage: dstart [options] executable [params...]")
+	}
 
 	attr := new(os.ProcAttr)
-	attr.Dir = "/tmp"
-	attr.Env = []string{} // env
-	attr.Files = []*os.File{os.Stdin, os.Stdout, os.Stderr} // input/output/error
-	attr.Sys = new(syscall.SysProcAttr)
-	attr.Sys.Credential = new(syscall.Credential)
-	uid, _ := strconv.Atoi(u.Uid)
-	gid, _ := strconv.Atoi(u.Gid)
-	attr.Sys.Credential.Uid = uint32(uid)
-	attr.Sys.Credential.Gid = uint32(gid)
+	attr.Dir = wd
+	if len(env) > 0 {
+		attr.Env = append(os.Environ(), env...)
+	} else {
+		attr.Env = append(os.Environ())
+	}
 
-	p, err := os.StartProcess("go-import-server", []string{"20"}, attr)
+	in := os.Stdin
+	if input != "" {
+		f, err := os.Open(input)
+		if err != nil {
+			log.Fatalln("open input file error: ", err)
+		}
+		in = f
+	}
 
-	fmt.Println(p, err)
+	out := os.Stdout
+	if output != "" {
+		f, err := os.OpenFile(output, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+		if err != nil {
+			log.Fatalln("open output file error: ", err)
+		}
+		out = f
+	}
 
-	time.Sleep(50 * time.Second)
+	erroutput := os.Stderr
+	if erroroutput != "" {
+		f, err := os.OpenFile(erroroutput, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+		if err != nil {
+			log.Fatalln("open error output file error: ", err)
+		}
+		erroutput = f
+	}
+
+	if etoo {
+		erroutput = out
+	}
+
+	attr.Files = []*os.File{in, out, erroutput} // input/output/error
+
+	// user
+	if username != "" {
+		u, err := user.Lookup(username)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		attr.Sys = new(syscall.SysProcAttr)
+		attr.Sys.Credential = new(syscall.Credential)
+		uid, err := strconv.Atoi(u.Uid)
+		if err != nil {
+			log.Fatalln("strconv uid error: ", err)
+		}
+		gid, err := strconv.Atoi(u.Gid)
+		if err != nil {
+			log.Fatalln("strconv gid error: ", err)
+		}
+		attr.Sys.Credential.Uid = uint32(uid)
+		attr.Sys.Credential.Gid = uint32(gid)
+	}
+
+	p, err := os.StartProcess(flag.Args()[0], flag.Args(), attr)
+	signal.Ignore(syscall.SIGCHLD)
+
+	if err != nil {
+		log.Fatalln("start process error: ", err)
+	} else {
+		log.Println("start process. pid: ", p.Pid)
+	}
 }
